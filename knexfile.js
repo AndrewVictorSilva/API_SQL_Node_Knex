9eeaeee2317bc8c5d71f1628e2131519ce61bfc3
Andrew Silva <avsilva.andrew@gmail.com>
2021-03-31 21:17:37 -0300

    procedures & triggers

--- knexfile.js
+++ knexfile.js
@@ -15,5 +15,12 @@ module.exports = {
     seeds: {
       directory: `${__dirname}/src/database/seeds`
     }
+  },
+  onUpdateTrigger(table) {
+    return `CREATE TRIGGER ${table}_update_at
+    BEFORE UPDATE ON ${table}
+    FOR EACH ROW
+    EXECUTE PROCEDURE on_update_timestamp();    
+    `
   }
 };
